cfc52c7efb6be7b0cd26932c451d7ecdf69e511d
Tobias Friese <tobifriese@gmail.com>
2013-06-23 15:53:45 +0200

    renaming bootstrap.php to env.php for better understanding

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 
 // require & init
-require_once '../bootstrap.php';
+require_once '../env.php';
 require_once TOBY_ROOT.'/Toby.class.php';
 
 Toby::init(empty($_GET['r']) ? 'index' : $_GET['r'], Toby::$SCOPE_WEB);
--- run.php
+++ run.php
@@ -1,11 +1,11 @@
 <?php
 
 // require config
-require_once 'bootstrap.php';
+require_once 'env.php';
 require_once TOBY_ROOT.'/Toby.class.php';
 
 // init & run
 array_shift($argv);
     
 Toby::init();
-Toby::runAction(array_shift($argv), array_shift($argv), $argv);
+if(count($argv) >= 2) Toby::runAction(array_shift($argv), array_shift($argv), $argv);
