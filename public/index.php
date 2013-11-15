398172a3548b83d1c641935eb6f990ea00eec1eb
Tobias Friese <tobifriese@gmail.com>
2013-11-15 12:32:46 +0100

    updates bootstrap due to last restructures in toby core

--- public/index.php
+++ public/index.php
@@ -4,4 +4,4 @@
 require_once '../env.php';
 require_once TOBY_ROOT.'/Toby.class.php';
 
-Toby::init(empty($_GET['r']) ? 'index' : $_GET['r'], Toby::SCOPE_WEB);
+Toby::getInstance()->init(empty($_GET['r']) ? 'index' : $_GET['r'], Toby::SCOPE_WEB);
--- run.php
+++ run.php
@@ -7,5 +7,6 @@ require_once TOBY_ROOT.'/Toby.class.php';
 // init & run
 array_shift($argv);
     
-Toby::init();
-if(count($argv) >= 2) Toby::runAction(array_shift($argv), array_shift($argv), $argv);
+$toby = Toby::getInstance();
+$toby->init();
+if(count($argv) >= 2) $toby->runAction(array_shift($argv), array_shift($argv), $argv);
