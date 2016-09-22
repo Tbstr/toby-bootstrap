97fafb65ec95d19c7113af2fd6acc7c0bd568d93
Tobias Friese <tobifriese@gmail.com>
2016-09-22 15:42:09 +0200

    aligns to recent changes in toby core

--- public/themes/default/default.php
+++ public/themes/default/default.php
@@ -4,25 +4,30 @@ use Toby\Assets\Assets;
 
 //  This is the configuration file for your theme. Per default it is named the same way
 //  as your theme directory. You can write alternative configurations with different names.
-//  In this case you have to set the »themeFunction« parameter inside the toby.cfg.php.
+//  In this case you have to set the 'theme.function' parameter inside the toby config file.
 //
 //  Here you can execute all kind of code necessary for your project or theme. Usually you
 //  use this file to specify the assets globally or for specific pages as in the following
 //  example:
 //
-//  Assets::setStandard()
-//  ->addCSS('css/standard.css')
-//  ->addJavaScript('js/standard.js');
-//
-//  Assets::setResolvePath('index/home')
-//  ->addCSS('css/home.css')
-//  ->addJavaScript('js/home.js');
-//
-//  Assets::setResolvePathDefault()
+//  Assets::byDefault()
 //  ->addCSS('css/default.css')
 //  ->addJavaScript('js/default.js');
+//
+//  Assets::forLayout('default')
+//  ->addCSS('css/layout_default.css')
+//  ->addJavaScript('js/layout_default.js');
+//
+//  Assets::forResolvePath('index/home')
+//  ->addCSS('css/index_home.css')
+//  ->addJavaScript('js/index_home.js');
+//
+//  Assets::forResolvePathUnset()
+//  ->addCSS('css/resolve_default.css')
+//  ->addJavaScript('js/resolve_default.js');
 
-//  The version query will be appended to the link paths to control caching without renaming
-//  the files. Increase or change this value if you have updates.
-
-Assets::$cacheBuster = '1';
+//  You can also set a cachebuster to trigger a reload when you updated your assets. The cache buster string will be
+//  appended to the link paths to control caching without renaming the files. Increase or change this value
+//  if you have updates.
+//
+//  Assets::setCacheBuster('2016_08_23b');
