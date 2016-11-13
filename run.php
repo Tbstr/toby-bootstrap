98c46460c2da5eb6e142b0d7bb5a412e5cae0d47
Tobias Friese <tobifriese@gmail.com>
2016-11-13 15:55:01 +0100

    updates toby submodule

--- run.php
+++ run.php
@@ -10,5 +10,10 @@ require_once TOBY_ROOT.'/Toby.php';
 array_shift($argv);
 
 $toby = Toby::getInstance();
-$toby->init(false, Toby::SCOPE_LOCAL);
-if(count($argv) >= 2) $toby->runAction(array_shift($argv), array_shift($argv), $argv);
+$toby->init(null, Toby::SCOPE_LOCAL);
+
+if(count($argv) >= 2)
+{
+    $response = $toby->runAction(array_shift($argv), array_shift($argv), $argv);
+    if($response !== null) $response->send();
+}
